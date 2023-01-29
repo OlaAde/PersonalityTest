090757f9e25c3c05c672b93745d8fda89c3582ff
Emmanuel <emmanuel.oladipo@oi-devs.com>
2023-01-29 13:02:35 +0300

    Updated the QuestionDto to allow all-arguments-constructor

--- src/main/java/com/teamway/personalitytest/dto/QuestionDto.java
+++ src/main/java/com/teamway/personalitytest/dto/QuestionDto.java
@@ -1,10 +1,14 @@
 package com.teamway.personalitytest.dto;
 
 import com.teamway.personalitytest.entity.Question;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class QuestionDto {
     private String question;
     private String answer1;
