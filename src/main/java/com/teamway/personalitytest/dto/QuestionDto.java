36ed9852e60c45d7117c0ce40e1893105f8602ef
Emmanuel <emmanuel.oladipo@oi-devs.com>
2023-01-29 12:45:27 +0300

    Created the QuestionDto for data communication with the frontend and added the modelmapper dependency to simplify the conversion

---
+++ src/main/java/com/teamway/personalitytest/dto/QuestionDto.java
@@ -0,0 +1,22 @@
+package com.teamway.personalitytest.dto;
+
+import com.teamway.personalitytest.entity.Question;
+import lombok.Data;
+import org.modelmapper.ModelMapper;
+
+@Data
+public class QuestionDto {
+    private String question;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+    private Integer answer1weight;
+    private Integer answer2weight;
+    private Integer answer3weight;
+    private Integer answer4weight;
+
+    public static QuestionDto buildFrom(Question question) {
+        return new ModelMapper().map(question, QuestionDto.class);
+    }
+}
